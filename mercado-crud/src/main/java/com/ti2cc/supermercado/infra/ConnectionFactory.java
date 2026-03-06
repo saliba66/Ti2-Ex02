48ba465721cf0bd5d773576d70c92f129ad63a7d
Gabriel Saliba <gabrielsaliba2@gmail.com>
2026-03-06 11:08:27 -0300

    .

--- mercado-crud/src/main/java/com/ti2cc/supermercado/infra/ConnectionFactory.java
+++ mercado-crud/src/main/java/com/ti2cc/supermercado/infra/ConnectionFactory.java
@@ -7,11 +7,11 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/mercado";
-    private static final String USER = "ti2cc";     // <<< ajuste para seu usuário do PostgreSQL
-    private static final String PASSWORD = "ti@cc"; // <<< ajuste para sua senha
+    private static final String USER = "ti2cc";     // <<< ajuste usuário do PostgreSQL
+    private static final String PASSWORD = "ti@cc"; // <<< ajuste  senha
 
     public static Connection getConnection() throws SQLException {
-        // Para JDBC 4+ não precisa do Class.forName
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+
